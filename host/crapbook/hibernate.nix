48dcf7dbb6e3bf51ea85d50b1dc237e1b5af584d
vivivitus <vivi_vitus@hotmail.com>
2024-11-29 21:36:58 +0000

    Workaround for non functioning wifi after resume

--- host/crapbook/hibernate.nix
+++ host/crapbook/hibernate.nix
@@ -1,6 +1,14 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
+  systemd.services.wifi-workaround = { 
+    description = "Workaround for Intel BE200 Wifi (broken after resume)";
+    wantedBy = [ "post-resume.target" ];
+    after = [ "post-resume.target" ];
+    script = "${pkgs.kmod}/bin/rmmod iwlmvm iwlwifi && ${pkgs.kmod}/bin/modprobe iwlwifi iwlmvm";
+    serviceConfig.Type = "oneshot";
+  };
+
   boot.kernelParams = [
   "resume=UUID=a2aa52a9-13ef-45a3-8711-8e0755772aa9" # findmnt -o UUID --noheadings /swap/
   "resume_offset=533760" # btrfs inspect-internal map-swapfile -r /swap/swapfile
