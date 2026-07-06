411f0c0efde4c43e9381c215894c09399c7f7244
vivivitus <vivi_vitus@hotmail.com>
2026-07-06 22:37:05 +0200

    firefox extensions

--- home/vivian/features/gui/firefox.nix
+++ home/vivian/features/gui/firefox.nix
@@ -1,8 +1,26 @@
-{ config, ... }:
+{ config, pkgs, inputs, ... }: {
 
-{
   programs.firefox = {
     enable = true;
     configPath = "${config.xdg.configHome}/mozilla/firefox";
+
+    profiles.vivian = {
+      id = 0;
+      name = "vivian";
+      isDefault = true;
+
+      # HIER DIE ÄNDERUNG: .packages anhängen
+      extensions.packages = let
+        addons = inputs.firefox-addons.packages.${pkgs.system};
+      in [
+        addons.ublock-origin
+        addons.canvasblocker
+        addons.gsconnect
+      ];
+
+      settings = {
+        "extensions.autoDisableScopes" = 0;
+      };
+    };
   };
 }
